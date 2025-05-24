84f8fbc49a2e45e610a5619a0877e9b7807822c6
Savinthi Vipulma <vithanagesavinthi@gmail.com>
2025-05-24 20:47:24 +0530

    initial commit

--- src/main/java/lk/ijse/desktop/myfx/myfinalproject/Controller/DashboardController.java
+++ src/main/java/lk/ijse/desktop/myfx/myfinalproject/Controller/DashboardController.java
@@ -4,9 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -101,7 +104,15 @@ public class DashboardController implements Initializable {
     }
 
     public void btnGoLogoutOnAction(ActionEvent actionEvent) throws IOException {
-        ancMainContainer.getChildren().clear();
-        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/View/LoginView.fxml"));
+        Stage stage = (Stage) ancMainContainer.getScene().getWindow();
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("/View/LoginView.fxml"));
+        Scene scene = new Scene(root);
+
+
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
 }
